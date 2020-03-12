408b2434435d0f18be40762f74a82a506f86833b
MarkoAT <no-reply@ixian.io>
2020-03-12 05:51:29 +0100

    - added rebuild script for Ubuntu/*nix

---
+++ rebuild.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+echo Rebuilding Ixian DLT...
+echo Cleaning previous build
+msbuild DLTNode.sln /p:Configuration=Release /target:Clean
+echo Removing packages
+rm -rf packages
+echo Restoring packages
+nuget restore DLTNode.sln
+echo Building DLT
+msbuild DLTNode.sln /p:Configuration=Release
+echo Cleaning Argon2
+cd Argon2_C
+make clean
+echo Building Argon2
+make
+echo Copying Argon2 library to release directory
+cp libargon2.so.1 ../IxianDLT/bin/Release/libargon2.so
+cd ..
+echo Done rebuilding Ixian DLT
