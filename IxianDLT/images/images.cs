4dfafc6142bdd3077c392e682f264f21feea3083
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2020-09-01 15:56:55 +0200

    Created images.cs

---
+++ IxianDLT/images/images.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace images() {
+     
+     class Backgrounds() {
+          
+          int blueBackgroundJpgFile = "./blue-background.jpg";
+          int darkBackgroundJpgFile = "./dark-background.jpg";
+          int whiteBackgroundJpgFile = "./white-background.jpg";
+          
+     };
+     
+};
