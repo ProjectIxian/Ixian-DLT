c16e0453c96f88f0f4bd810b6fbe88c15d69a3be
MarkoAT <no-reply@ixian.io>
2020-09-19 15:39:01 +0200

    xdc-0.7.0

--- IxianDLT/Meta/Config.cs
+++ IxianDLT/Meta/Config.cs
@@ -70,7 +70,7 @@ namespace DLT
             public static bool disableChainReorg = false;
 
             // Read-only values
-            public static readonly string version = "xdc-0.7.0-rc2"; // DLT Node version
+            public static readonly string version = "xdc-0.7.0"; // DLT Node version
 
             public static readonly string checkVersionUrl = "https://www.ixian.io/update.txt";
             public static readonly int checkVersionSeconds = 6 * 60 * 60; // 6 hours
